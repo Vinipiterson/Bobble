825d9743dd3f52118d7dcc381a694416b1f37f40
Vinipiterson <Vinipiassini123@gmail.com>
2025-01-24 18:17:59 -0300

    Initial Commit

---
+++ Source/Bobble/BobblePlayerController.h
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Templates/SubclassOf.h"
+#include "GameFramework/PlayerController.h"
+#include "BobblePlayerController.generated.h"
+
+/** Forward declaration to improve compiling times */
+class UNiagaraSystem;
+class UInputMappingContext;
+class UInputAction;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+
+UCLASS()
+class ABobblePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ABobblePlayerController();
+	
+	// To add mapping context
+	virtual void BeginPlay();
+};
+
+
