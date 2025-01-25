b5c756ec4045d7123c4bdae9d51764dd11921644
Vinipiterson <Vinipiassini123@gmail.com>
2025-01-25 15:45:38 -0300

    Finished level end, added padding to buttons, created game instance and save system

---
+++ Source/Bobble/Bobble.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Bobble.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Bobble, "Bobble" );
---
+++ Source/Bobble/Private/A.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "A.h"
+
+// Sets default values
+AA::AA()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AA::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AA::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
